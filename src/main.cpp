bc2db048b6653663beaaf3968f2569c70e203d12
Q <a4.fun@mail.ru>
2026-03-17 03:18:18 +0300

    тест вывод времени 1 включение запрос по wifi 2 и тд RTC вывод  Время, Время прошедшее с послед синхры кол во перерисовки дисплея время радоты от просыпа до подготовки буфера вывода на экран

--- src/main.cpp
+++ src/main.cpp
@@ -3,58 +3,94 @@
 #include <GxEPD2_BW.h>
 #include <U8g2_for_Adafruit_GFX.h>
 #include <WiFi.h>
-#include <time.h>
+#include "time.h"
 
-// Параметры: CS=7, DC=9, RST=2, BUSY=0
+#define EPD_POWER_PIN 1
+
 GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(7, 9, 2, 0));
 U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;
 
-const char* ssid = "MedVed";
+// Переменные в RTC
+RTC_DATA_ATTR bool initialSyncDone = false;
+RTC_DATA_ATTR int bootCount = 0;
+RTC_DATA_ATTR time_t lastSyncTime = 0; // Время последней синхронизации
+
+const char* ssid     = "MedVed";
 const char* password = "19411945";
+const long  gmtOffset_sec = 60*60*3;
+const int   daylightOffset_sec = 0;
 
 void setup() {
-  Serial.begin(115200);
+    pinMode(EPD_POWER_PIN, OUTPUT);
+    digitalWrite(EPD_POWER_PIN, HIGH); 
+    delay(100); 
 
-  // Инициализация SPI и дисплея
-  SPI.begin(4, -1, 6, 7); // SCK=6, MOSI=4, CS=7
-  display.init(115200);
-  u8g2Fonts.begin(display);
-  u8g2Fonts.setFont(u8g2_font_unifont_t_cyrillic);
-  u8g2Fonts.setForegroundColor(GxEPD_BLACK);
-  u8g2Fonts.setBackgroundColor(GxEPD_WHITE);
-  u8g2Fonts.setFontMode(1);
-  // Подключение к Wi-Fi
-  WiFi.begin(ssid, password);
-  unsigned long startAttempt = millis();
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-  // Получение времени
-  configTime(10800, 0, "pool.ntp.org");
-  struct tm timeinfo;
-  if (getLocalTime(&timeinfo)) {
-    char t1[10];
-    strftime(t1, sizeof(t1), "%H:%M:%S", &timeinfo);
+    unsigned long startMillis = millis();
+    Serial.begin(115200);
+    bootCount++;
 
+    SPI.begin(4, -1, 6, 7); 
+    display.init(115200);
+    u8g2Fonts.begin(display);
+    u8g2Fonts.setFont(u8g2_font_unifont_t_cyrillic);
+    u8g2Fonts.setForegroundColor(GxEPD_BLACK);
+    u8g2Fonts.setBackgroundColor(GxEPD_WHITE);
+    u8g2Fonts.setFontMode(1);
+
+    configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org");
+    struct tm timeinfo;
+    bool wifiUsed = false;
+
+    if (!initialSyncDone) {
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) { delay(500); }
+        if (getLocalTime(&timeinfo)) {
+            initialSyncDone = true;
+            lastSyncTime = mktime(&timeinfo); // Сохраняем время синхронизации
+        }
+        wifiUsed = true;
+        WiFi.disconnect(true);
+        WiFi.mode(WIFI_OFF);
+    }
+    
+    getLocalTime(&timeinfo);
+    time_t now = mktime(&timeinfo);
+    
+    // Вычисляем время, прошедшее с момента синхронизации (в секундах)
+    long secondsSinceSync = (lastSyncTime > 0) ? (now - lastSyncTime) : 0;
+    long totalSeconds = secondsSinceSync;
+    int days = totalSeconds / 86400;
+    int hours = (totalSeconds % 86400) / 3600;
+    int minutes = (totalSeconds % 3600) / 60;
+    char timeStr[15];
+    strftime(timeStr, sizeof(timeStr), "%H:%M:%S", &timeinfo);
+    
+    // 3. Отрисовка
     display.setFullWindow();
     display.firstPage();
     do {
-      display.fillScreen(GxEPD_WHITE);
-      u8g2Fonts.setCursor(10, 40);
-      u8g2Fonts.print("Время: ");
-      u8g2Fonts.print(t1);
+        display.fillScreen(GxEPD_WHITE);
+        u8g2Fonts.setCursor(10, 30);
+        u8g2Fonts.print(wifiUsed ? "Статус: NTP" : "Статус: RTC");
+        u8g2Fonts.setCursor(10, 55);
+        u8g2Fonts.print("Время: "); u8g2Fonts.print(timeStr);
+        u8g2Fonts.setCursor(10, 80);
+        u8g2Fonts.print("syn: ");
+        u8g2Fonts.print(days); u8g2Fonts.print("д ");
+        u8g2Fonts.print(hours); u8g2Fonts.print("ч ");
+        u8g2Fonts.print(minutes); u8g2Fonts.print("м");
+        u8g2Fonts.setCursor(10, 105);
+        u8g2Fonts.print("Проходов: "); u8g2Fonts.print(bootCount);
+        u8g2Fonts.setCursor(10, 130);
+        u8g2Fonts.print("Работа: "); u8g2Fonts.print(millis() - startMillis); u8g2Fonts.print(" мс");
     } while (display.nextPage());
-  }
 
-  // Завершение работы
-  Serial.println("Уходим в сон...");
-  Serial.flush(); 
-  
-  WiFi.disconnect(true);
-  esp_sleep_enable_timer_wakeup(60 * 1000000); // 60 секунд
-  esp_deep_sleep_start();
+    display.hibernate();
+    pinMode(4, INPUT); pinMode(6, INPUT); pinMode(7, INPUT); 
+    digitalWrite(EPD_POWER_PIN, LOW);
+
+    esp_sleep_enable_timer_wakeup(60 * 1000000);
+    esp_deep_sleep_start();
 }
 
 void loop() {}
